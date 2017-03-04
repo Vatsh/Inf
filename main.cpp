c952e684a22cbcc1046e69910df3ce6df00249f1
Perfomer <perfomeria@gmail.com>
2017-03-04 15:03:19 +0700

    Update (v2)
    * Added formula: average lenght of codeword, minimal bitness of
    codeword.
    * Minor bugs fixed.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,22 @@
 #include "stdio.h"
 #include "locale.h"
 
-// хорошим стилем является наименование функций глаголом. В данном случае название начинается с get
 int getPositiveInteger();
+int getBitness(int srcAlphabetLength, int codeAlphabetLength);
 float getDouble(double boundLower, double boundUpper);
 double getEntropyEq(int n);
 double getEntropyNotEq(int n);
 double getRedundancy(double entropyEq, double entropy);
+double getAverageLenghtCodeword(int n);
 
 int main()
 {
     setlocale(LC_CTYPE, "rus");
-    int variant = 0, n = 0;
-    double entropyEq = 0, entropy = 0, redundancy = 0;
+    int variant = 0, alphabetSrcLenght = 0, alphabetCodeLenght = 0, bitness = 0;
+    double entropyEq = 0, entropy = 0, redundancy = 0, avrgLenghtCodeword = 0;
 
     printf("%s\n", "Введите размер алфавита");
-    n = getPositiveInteger();
+    alphabetSrcLenght = getPositiveInteger();
 
     do
     {
@@ -24,7 +25,11 @@ int main()
         printf("%s\n", "|1. Энтропия (равновероятная).  |");
         printf("%s\n", "|2. Энтропия (неравновероятная).|");
         printf("%s\n", "|3. Избыточность.               |");
-        printf("%s\n", "|4. Выход.                      |");
+        printf("%s\n", "|                               |");
+        printf("%s\n", "|4. Разрядность.                |");
+        printf("%s\n", "|5. Средняя длина код. слова.   |");
+        printf("%s\n", "|                               |");
+        printf("%s\n", "|6. Выход.                      |");
         printf("%s\n", " -------------------------------");
 
         printf("%s\n", "Выберите действие: ");
@@ -32,11 +37,11 @@ int main()
         switch(variant)
         {
             case 1:
-                entropyEq = getEntropyEq(n);
+                entropyEq = getEntropyEq(alphabetSrcLenght);
                 printf("Энтропия H = %lf бит/эл\n", entropyEq);
                 break;
             case 2:
-                entropy = getEntropyNotEq(n);
+                entropy = getEntropyNotEq(alphabetSrcLenght);
                 printf("Энтропия H = %lf бит/эл\n", entropy);
                 break;
             case 3:
@@ -47,12 +52,22 @@ int main()
                     printf("Избыточность алфавита: %lf\n", redundancy);
                 break;
             case 4:
+                printf("%s\n", "Введите размер кодового алфавита");
+                alphabetCodeLenght = getPositiveInteger();
+                bitness = getBitness(alphabetSrcLenght, alphabetCodeLenght);
+                printf("Минимальная разрядность кодового слова r = %d симв.\n", bitness);
+                break;
+            case 5:
+                avrgLenghtCodeword = getAverageLenghtCodeword(alphabetSrcLenght);
+                printf("Средняя длина кодового слова l = %lf симв.\n", avrgLenghtCodeword);
+                break;
+            case 6:
                 printf("%s\n", "Завершение...");
                 break;
             default:
                 printf("%s\n", "Введите, пожалуйста, существующий пункт меню.");
         }
-    } while(variant != 4);
+    } while(variant != 6);
     getchar();
 
     return 0;
@@ -87,7 +102,7 @@ double getEntropyNotEq(int n)
 {
     double entropy = 0;
 
-    printf("%s\n", "Введите ....  [пример: 0.09]");
+    printf("%s\n", "Введите ....  [пример: 0,09]");
     for (int i = 0; i < n; i++)
     {
         printf("p[%d] = ", i + 1);
@@ -105,3 +120,27 @@ double getRedundancy(double entropyEq, double entropy)
     return ((entropyEq - entropy) / entropyEq) * 100;
 }
 
+double getAverageLenghtCodeword(int n)
+{
+    double length = 0;
+
+    printf("%s\n", "Введите ....  [пример: 0,09]");
+    for (int i = 0; i < n; i++)
+    {
+        printf("l[%d] = ", i + 1);
+        int l = getPositiveInteger();
+        printf("p[%d] = ", i + 1);
+        double p = getDouble();
+        length = length + (l * p);
+    }
+    return length;
+}
+
+int getBitness(int srcAlphabet, int codeAlphabet)
+{
+    double bit = log(srcAlphabet) / log(codeAlphabet);
+    if (bit - (int)bit != 0)
+        return (int)bit + 1;
+    else
+        return bit;
+}
